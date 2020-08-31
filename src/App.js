1440cedf50bab0010afcca5e3ee1bf0de07492e9
Megan Horsfield <meganthadev@gmail.com>
2020-08-31 03:01:29 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -5,15 +5,15 @@ import { addItem } from  './actions/items';
 
 class App extends Component {
 
-  handleOnClick() {
-    this.props.dispatch(addItem());
+   handleOnClick = event => {
+    this.props.addItem()
   }
-
-  render() {\
+  
+  render() {
   debugger
     return (
       <div className="App">
-        <button onClick={(event) => this.handleOnClick(event)}>
+        <button onClick={this.handleOnClick}>
           Click
           </button>
         <p>{this.props.items.length}</p>
